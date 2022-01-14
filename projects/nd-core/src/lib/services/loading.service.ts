59ba9988f2b4a11c0539819bd15a6d48be2d1663
Simone Gosetto <46279708+simonegosetto@users.noreply.github.com>
2022-01-14 17:50:42 +0100

    implementazione loading service
    implementazione loading service per gestione della coda di chiamate

--- projects/nd-core/src/lib/services/loading.service.ts
+++ projects/nd-core/src/lib/services/loading.service.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
+import { BehaviorSubject, Subject } from 'rxjs';
+
+export interface LoaderState {
+  show: boolean;
+}
 
 @Injectable({
     providedIn: 'root'
 })
 export class LoadingService {
 
-    isLoading = false;
-
     constructor(public loadingController: LoadingController) { }
+  
+    private loaderSubject = new BehaviorSubject<LoaderState>({ show: undefined});
+    private progressSubject = new Subject<LoaderState>();
+
+    /**
+     * Contains in-progress loading requests
+     */
+    public isLoading = false;
+    public loaderState = this.loaderSubject.asObservable();
+    public progressState = this.progressSubject.asObservable();
+    public loadingMap: Map<string, boolean> = new Map<string, boolean>();
 
     async present(duration = 2000) {
         this.isLoading = true;
@@ -29,5 +43,37 @@ export class LoadingService {
     async dismiss() {
         this.isLoading = false;
         return await this.loadingController.dismiss().then(() => '' ); // console.log('dismissed'));
+    }
+  
+    /**
+     * Sets the loadingSub property value based on the following:
+     * - If loading is true, add the provided url to the loadingMap with a true value, set loadingSub value to true
+     * - If loading is false, remove the loadingMap entry and only when the map is empty will we set loadingSub to false
+     * This pattern ensures if there are multiple requests awaiting completion, we don't set loading to false before
+     * other requests have completed. At the moment, this function is only called from the @link{HttpRequestInterceptor}
+     * param loading {boolean}
+     * param url {string}
+     */
+    setLoading(loading: boolean, url: string): void {
+      if (!url) {
+        throw new Error('The request URL must be provided to the LoadingService.setLoading function');
+      }
+      if (loading === true) {
+        this.loadingMap.set(url, loading);
+        this.loaderSubject.next({show: true});
+      }else if (loading === false && this.loadingMap.has(url)) {
+        this.loadingMap.delete(url);
+      }
+      if (this.loadingMap.size === 0) {
+        this.loaderSubject.next({show: false});
+      }
+    }
+
+    public showProgress() {
+      this.progressSubject.next({show: true});
+    }
+
+    public hideProgress() {
+      this.progressSubject.next({show: false});
     }
 }
