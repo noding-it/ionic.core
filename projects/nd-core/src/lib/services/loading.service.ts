2207d3c4b9ed88e19919d791d9dcda90c12c7cd2
Simone Gosetto <simone.gosetto@gmail.com>
2022-01-16 22:07:31 +0100

    modifiche su loader

--- projects/nd-core/src/lib/services/loading.service.ts
+++ projects/nd-core/src/lib/services/loading.service.ts
@@ -1,33 +1,29 @@
-import { Injectable } from '@angular/core';
-import { LoadingController } from '@ionic/angular';
+import {Injectable} from '@angular/core';
+import {LoadingController} from '@ionic/angular';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class LoadingService {
 
-    isLoading = false;
+  isLoading = false;
 
-    constructor(public loadingController: LoadingController) { }
+  constructor(public loadingController: LoadingController) {
+  }
 
-    async present(duration = 2000) {
-        this.isLoading = true;
-        return await this.loadingController.create({
-            spinner: null,
-            duration,
-            cssClass: 'custom-loading'
-        }).then(a => {
-            a.present().then(() => {
-                // console.log('presented');
-                if (!this.isLoading) {
-                    a.dismiss().then(() => '' ); // console.log('abort presenting'));
-                }
-            });
-        });
-    }
+  async present(duration = 2000) {
+    this.isLoading = true;
+    const response = await this.loadingController.create({
+      spinner: null,
+      duration,
+      cssClass: 'custom-loading'
+    })
+    return response.present();
 
-    async dismiss() {
-        this.isLoading = false;
-        return await this.loadingController.dismiss().then(() => '' ); // console.log('dismissed'));
-    }
+  }
+
+  async dismiss() {
+    this.isLoading = false;
+    return await this.loadingController.dismiss(); // console.log('dismissed'));
+  }
 }
