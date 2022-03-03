7c115aab3e8909851bd828a69a82748dbb19d1ef
Arkky97 <antonioxperia97@gmail.com>
2022-03-03 14:26:19 +0100

    "version": "0.1.21" - Fix di 4rya.service.ts.

--- projects/nd-core/src/lib/services/4rya.service.ts
+++ projects/nd-core/src/lib/services/4rya.service.ts
@@ -12,8 +12,8 @@ export class AryaService {
   ) {
   }
 
-  public update4ryaID(aryaID: string): Observable<any> {
-    return this._gs.callGateway('Worx1sPzYhyh/h+S02GPikq6p3ev7Aq6zvfd4FRt38stWy0tSVYtWy2PKRmY1Mk/nnZIT0MAKvy29PNTPQ7XjJVefIrKOIkiXw@@', `'${localStorage.getItem('token')}','${aryaID}'`, false);
+  public update4ryaID(aryaID: string, aryaPK: string): Observable<any> {
+    return this._gs.callGateway('Worx1sPzYhyh/h+S02GPikq6p3ev7Aq6zvfd4FRt38stWy0tSVYtWy2PKRmY1Mk/nnZIT0MAKvy29PNTPQ7XjJVefIrKOIkiXw@@', `'${localStorage.getItem('token')}','${aryaID}','${aryaPK}'`, false);
   }
 
 }
